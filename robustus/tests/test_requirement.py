92f8a788becba27b7bb5d80369ccc09062964960
Oleg Sinyavskiy <sinyavskiy@braincorporation.com>
2013-09-16 22:33:41 -0700

    move tests to test directory

---
+++ robustus/tests/test_requirement.py
@@ -0,0 +1,12 @@
+# =============================================================================
+# COPYRIGHT 2013 Brain Corporation.
+# License under MIT license (see LICENSE file)
+# =============================================================================
+
+
+import doctest
+import robustus
+
+
+if __name__ == '__main__':
+    doctest.testmod(robustus.detail.requirement)
