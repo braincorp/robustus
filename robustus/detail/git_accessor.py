f87ac7cc5e44ba2025807eac7fb528ab18ef39d8
Oleg Sinyavskiy <sinyavskiy@braincorporation.com>
2013-09-17 19:31:16 -0700

    git file accessor

---
+++ robustus/detail/git_accessor.py
@@ -0,0 +1,28 @@
+# =============================================================================
+# COPYRIGHT 2013 Brain Corporation.
+# License under MIT license (see LICENSE file)
+# =============================================================================
+
+
+import subprocess
+import tempfile
+import shutil
+import os
+
+
+class GitAccessor(object):
+    def access(self, repo_link, tag, path_to_file):
+        '''
+        Checksout a single file from git repo specified
+        by 'repo_link' from branch 'tag' under path 'path_to_file'.
+        @return: file content 
+        '''
+        tmp_dir = tempfile.mkdtemp()
+        if tag is not None:
+            subprocess.call(['git', 'clone', '-b', tag, repo_link, tmp_dir])
+        else:
+            subprocess.call(['git', 'clone', repo_link, tmp_dir])
+        with open(os.path.join(tmp_dir, path_to_file)) as file:
+            lines = file.readlines()
+        shutil.rmtree(tmp_dir)
+        return lines
