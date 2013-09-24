78dfee73e2c2b853fe0fec49cd23e5399d71da6e
Oleg Sinyavskiy <sinyavskiy@braincorporation.com>
2013-09-24 16:07:25 -0700

    mac safe cp

--- robustus/detail/install_panda3d.py
+++ robustus/detail/install_panda3d.py
@@ -96,32 +96,41 @@ def install(robustus, requirement_specifier, rob_file, ignore_index):
 
     if in_cache():
         # install panda3d to virtualenv
-        run_shell('cp %s/bin/* %s/bin' % (panda_install_dir, robustus.env))
+
+        def shell_cp(src_and_dist):
+            # mac cp doesn't have --update flag
+            if sys.platform.startswith('darwin'):
+                cp_update_flag = ''
+            else:
+                cp_update_flag = ' -u'
+            run_shell('cp -r%s -p %s' % (cp_update_flag, src_and_dist))
+
+        shell_cp('%s/bin/* %s/bin' % (panda_install_dir, robustus.env))
         incdir = os.path.join(robustus.env, 'include/panda3d')
         shutil.rmtree(incdir, ignore_errors=True)
         os.mkdir(incdir)
-        run_shell('cp -r -u -p %s/include/* %s/' % (panda_install_dir, incdir))
+        shell_cp('%s/include/* %s/' % (panda_install_dir, incdir))
         libdir = os.path.join(robustus.env, 'lib/panda3d')
         shutil.rmtree(libdir, ignore_errors=True)
         os.mkdir(libdir)
-        run_shell('cp -r -u -p %s/lib/* %s/' % (panda_install_dir, libdir))
+        shell_cp('%s/lib/* %s/' % (panda_install_dir, libdir))
         env_sharedir = os.path.join(robustus.env, 'share')
         if not os.path.isdir(env_sharedir):
             os.mkdir(env_sharedir)
         sharedir = os.path.join(env_sharedir, 'panda3d')
         shutil.rmtree(sharedir, ignore_errors=True)
         os.mkdir(sharedir)
-        run_shell('cp -r -u -p %s/direct/* %s/' % (panda_install_dir, sharedir))
-        run_shell('cp -r -u -p %s/models/* %s/' % (panda_install_dir, sharedir))
-        run_shell('cp -r -u -p %s/pandac/* %s/' % (panda_install_dir, sharedir))
-        run_shell('cp -u %s/lib/panda3d.py %s/' % (panda_install_dir, sharedir))
+        shell_cp('%s/direct/* %s/' % (panda_install_dir, sharedir))
+        shell_cp('%s/models/* %s/' % (panda_install_dir, sharedir))
+        shell_cp('%s/pandac/* %s/' % (panda_install_dir, sharedir))
+        shell_cp('%s/lib/panda3d.py %s/' % (panda_install_dir, sharedir))
         env_etcdir = os.path.join(robustus.env, 'etc')
         if not os.path.isdir(env_etcdir):
             os.mkdir(env_etcdir)
         etcdir = os.path.join(env_etcdir, 'panda3d')
         shutil.rmtree(etcdir, ignore_errors=True)
         os.mkdir(etcdir)
-        run_shell('cp -r -u -p %s/etc/* %s/' % (panda_install_dir, etcdir))
+        shell_cp('%s/etc/* %s/' % (panda_install_dir, etcdir))
 
         write_file(os.path.join(robustus.env, 'lib/python2.7/site-packages/panda3d.pth'),
                    'w',
