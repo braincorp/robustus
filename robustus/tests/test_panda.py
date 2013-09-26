935a1620bd9905e9b0b1d6806748b70d15b6d616
dmitry.trifonov <trifonov@braincorporation.com>
2013-09-26 13:39:28 +0400

    testing availability of panda modules

--- robustus/tests/test_panda.py
+++ robustus/tests/test_panda.py
@@ -10,10 +10,16 @@ from robustus.detail import perform_standard_test
 
 def test_panda_installation(tmpdir):
     tmpdir.chdir()
-    perform_standard_test('panda3d==bc1', ['panda3d'], [], ['bullet==bc1'])
+
+    panda_modules = ['panda3d',
+                     'panda3d.core'
+                     'panda3d.bullet']
+
+    perform_standard_test('panda3d==bc1', panda_modules, [], ['bullet==bc1'])
     # need bison to build panda 1.8.1
     if os.path.isfile('/usr/bin/bison'):
-        perform_standard_test('panda3d==1.8.1', ['panda3d'], [], ['bullet==bc1'])
+        perform_standard_test('panda3d==1.8.1', panda_modules, [], ['bullet==bc1'])
+
 
 if __name__ == '__main__':
     pytest.main('-s %s -n0' % __file__)
