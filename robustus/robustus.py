dd6967a8575d1ae398ee17ce54a04c82c2c4df78
Oleg Sinyavskiy <sinyavskiy@braincorporation.com>
2013-10-02 13:54:47 -0700

    manual check

--- robustus/robustus.py
+++ robustus/robustus.py
@@ -245,7 +245,12 @@ class Robustus(object):
 
             # here we have to run via shell because requirement can be editable and then it will require
             # extra parsing to extract -e flag into separate argument.
-            command = ' '.join([self.pip_executable, '--exists-action=i', 'install', requirement_specifier.freeze()])
+            if requirement_specifier.url is not None:
+                editable_requirement_paht = os.path.join(self.env, 'src', requirement_specifier.name)
+                logging.info('Got url-based requirement. Checking if exists %s ' % (editable_requirement_paht,))
+                if os.path.exists(editable_requirement_paht):
+                    raise Exception('path %s' % os.path.join(self.env, 'src', requirement_specifier.name))
+            command = ' '.join([self.pip_executable, 'install', requirement_specifier.freeze()])
             logging.info('Got url-based requirement. '
                          'Fall back to pip shell command:%s' % (command,))
             run_shell(command)
