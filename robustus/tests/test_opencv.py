9dce46a3b6cf0dca8e32fbc8d8246de05f2d81ee
Oleg Sinyavskiy <sinyavskiy@braincorporation.com>
2013-11-19 17:16:31 -0800

    skip opencv 247 on travis

--- robustus/tests/test_opencv.py
+++ robustus/tests/test_opencv.py
@@ -8,8 +8,17 @@ import logging
 from robustus.detail import perform_standard_test
 
 
-@pytest.mark.parametrize("requirement", ['OpenCV==2.4.4', 'OpenCV==2.4.7'])
-def test_opencv_installation(tmpdir, requirement):
+def test_opencv_installation_244(tmpdir):
+    _do_test_opencv_installation(tmpdir, 'OpenCV==2.4.4')
+
+
+@pytest.mark.skipif("'TRAVIS' in os.environ",
+                    reason="https://braincorporation.atlassian.net/browse/ROB-21")
+def test_opencv_installation_247(tmpdir):
+    _do_test_opencv_installation(tmpdir, 'OpenCV==2.4.7')
+
+
+def _do_test_opencv_installation(tmpdir, requirement):
     logging.getLogger().setLevel(logging.INFO)
     tmpdir.chdir()
 
