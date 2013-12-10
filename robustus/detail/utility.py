d8e9a1a38cb3da5536992fa9b39d5ebf2661ce26
trifonov-bc <trifonov@braincorporation.com>
2013-12-10 12:41:19 -0800

    add which function

--- robustus/detail/utility.py
+++ robustus/detail/utility.py
@@ -53,6 +53,25 @@ def ln(src, dst, force=False):
     os.symlink(src, dst)
 
 
+def which(program):
+    import os
+    def is_exe(fpath):
+        return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+
+    fpath, fname = os.path.split(program)
+    if fpath:
+        if is_exe(program):
+            return program
+    else:
+        for path in os.environ["PATH"].split(os.pathsep):
+            path = path.strip('"')
+            exe_file = os.path.join(path, program)
+            if is_exe(exe_file):
+                return exe_file
+
+    return None
+
+
 def download(url, filename=None):
     """
     download file from url, store it under name
