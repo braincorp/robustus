5cd20f54663f650a714568be3726c0027057b684
Jonathan J Hunt <hunt@braincorporation.com>
2014-01-28 16:15:28 -0800

    perrepo test seems to be working.

--- robustus/tests/test_robustus.py
+++ robustus/tests/test_robustus.py
@@ -53,5 +53,47 @@ def test_robustus(tmpdir):
 
     shutil.rmtree(test_env)
 
+
+
+def test_pereditable(tmpdir):
+    """Create a package with some editable requirements and check
+    that perrepo runs as expected."""
+    base_dir = str(tmpdir.mkdir('test_perrepo_env'))
+    test_env = os.path.join(base_dir, 'env')
+    working_dir = os.path.join(base_dir, 'working_dir')
+    repo_dir = os.path.join(base_dir, 'repo_dir')
+    editable_sources = ['a', 'b']
+
+    # create env and install some packages
+    logging.info('creating ' + test_env)
+    os.mkdir(working_dir)
+    os.chdir(working_dir)
+    os.system('git init .')
+    robustus.execute(['env', test_env])
+
+    # create some other git repos
+    os.mkdir(repo_dir)
+    for r in editable_sources:
+        r_dir = os.path.join(repo_dir, r)
+        os.mkdir(r_dir)
+        os.system('cd "%s" && git init .' % r_dir)
+
+    os.chdir(working_dir)
+    robustus_executable = os.path.join(test_env, 'bin/robustus')
+    test_requirements = os.path.join(working_dir, 'test_requirements1.txt')
+    with open(test_requirements, 'w') as file:
+        for r in editable_sources:
+            r_dir = os.path.join(repo_dir, r)
+            file.write('-e %s\n' % r_dir)
+
+    subprocess.call([robustus_executable, 'install', '-r', test_requirements])
+
+    # Now check that robustus behaves as expected
+    subprocess.call([robustus_executable, 'perrepo', 'touch', 'foo'])
+    assert os.path.exists(os.path.join(working_dir, 'foo'))
+    for r in editable_sources:
+        assert os.path.exists(os.path.join(test_env, 'src', r, 'foo'))
+
+
 if __name__ == '__main__':
     pytest.main('-s %s -n0' % __file__)
