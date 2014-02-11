ad491a136cbd6e27932ac1136ee8e4a9ddf8902d
Oleg Sinyavskiy <sinyavskiy@braincorporation.com>
2014-02-11 14:45:58 -0800

    more paths

--- robustus/detail/install_pyside.py
+++ robustus/detail/install_pyside.py
@@ -30,18 +30,17 @@ def install(robustus, requirement_specifier, rob_file, ignore_index):
             else:
                 raise RequirementException('System-wide PySide is missing, run: brew install pyside')   
         else:
-            if os.path.isfile('/usr/lib64/python2.7/site-packages/PySide/QtCore.so'):
-                logging.info('Linking pyside on centos')
-                ln('/usr/lib64/python2.7/site-packages/sip.so',
-                    os.path.join(robustus.env, 'lib/python2.7/site-packages/sip.so'), force = True)
-                ln('/usr/lib64/python2.7/site-packages/PySide',
-                    os.path.join(robustus.env, 'lib/python2.7/site-packages/PySide'), force = True)
-            elif os.path.isfile('/usr/lib/python2.7/dist-packages/PySide/QtCore.so'):
-                logging.info('Linking pyside on ubuntu')
-                ln('/usr/lib/python2.7/dist-packages/sip.so',
-                    os.path.join(robustus.env, 'lib/python2.7/site-packages/sip.so'), force = True)
-                ln('/usr/lib/python2.7/dist-packages/PySide',
-                    os.path.join(robustus.env, 'lib/python2.7/site-packages/PySide'), force = True)
+            candidates = ['/usr/lib64/python2.7/site-packages/',
+                          '/usr/lib/python2.7/dist-packages',
+                          '/usr/local/lib/python2.7/dist-packages']
+            for c in candidates:
+                if os.path.isfile(os.path.join(c, 'PySide/QtCore.so')):
+                    logging.info('Linking pyside in %s' % c)
+                    ln(os.path.join(c, 'sip.so'),
+                        os.path.join(robustus.env, 'lib/python2.7/site-packages/sip.so'), force = True)
+                    ln(os.path.join(c, 'PySide'),
+                        os.path.join(robustus.env, 'lib/python2.7/site-packages/PySide'), force = True)
+                    break
             else:
                 raise RequirementException('System-wide PySide is missing, run: sudo apt-get install python-pyside')
     else:
