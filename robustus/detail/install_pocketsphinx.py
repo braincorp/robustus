982ee0879f308ddbd57b4eeb55efd6d79ad56401
dmitry.trifonov <trifonov@braincorporation.com>
2014-05-05 22:01:30 +0400

    reinstall pocketsphinx each time, it can't be cached easily

--- robustus/detail/install_pocketsphinx.py
+++ robustus/detail/install_pocketsphinx.py
@@ -12,10 +12,22 @@ from requirement import RequirementException
 def install(robustus, requirement_specifier, rob_file, ignore_index):
     build_dir = os.path.join(robustus.cache, 'pocketsphinx-%s' % requirement_specifier.version)
 
-    def in_cache():
-        return os.path.isdir(build_dir)
+    # check if already installed
+    pocketsphinx = os.path.join(robustus.env, 'lib/python2.7/site-packages/pocketsphinx.so')
+    if os.path.isfile(pocketsphinx):
+        return
 
-    def configure():
+    # unfortunately we can't cache pocketsphinx, it has to be rebuild after reconfigure
+    cwd = os.getcwd()
+    try:
+        # build in cache
+        os.chdir(robustus.cache)
+        archive = robustus.download('pocketsphinx', requirement_specifier.version)
+        unpack(archive)
+
+        logging.info('Building pocketsphinx')
+        os.chdir(build_dir)
+
         sphinxbase_dir = os.path.join(robustus.cache, 'sphinxbase-%s/' % requirement_specifier.version)
         retcode = run_shell('./configure'
                             + (' --prefix=%s' % robustus.env)
@@ -27,37 +39,16 @@ def install(robustus, requirement_specifier, rob_file, ignore_index):
         if retcode != 0:
             raise RequirementException('pocketsphinx configure failed')
 
-    cwd = os.getcwd()
-    if not in_cache() and not ignore_index:
-        try:
-            # build in cache
-            os.chdir(robustus.cache)
-            archive = robustus.download('pocketsphinx', requirement_specifier.version)
-            unpack(archive)
+        retcode = run_shell(['make'], verbose=robustus.settings['verbosity'] >= 1)
+        if retcode != 0:
+            raise RequirementException('pocketsphinx build failed')
 
-            logging.info('Building pocketsphinx')
-            os.chdir(build_dir)
-
-            configure()
-
-            retcode = run_shell(['make'], verbose=robustus.settings['verbosity'] >= 1)
-            if retcode != 0:
-                raise RequirementException('pocketsphinx build failed')
-        except RequirementException:
-            safe_remove(build_dir)
-        finally:
-            safe_remove(archive)
-
-    if in_cache():
-        logging.info('Installing sphinxbase into virtualenv')
-        os.chdir(build_dir)
-        configure() # need to configure, because install path may change
+        logging.info('Installing pocketsphinx into virtualenv')
         retcode = run_shell('make install', shell=True, verbose=robustus.settings['verbosity'] >= 1)
-        os.chdir(cwd)
         if retcode != 0:
             raise RequirementException('sphinxbase install failed')
+
         # fix rpath for pocketsphinx
-        pocketsphinx = os.path.join(robustus.env, 'lib/python2.7/site-packages/pocketsphinx.so')
         fix_rpath(robustus.env, pocketsphinx, os.path.join(robustus.env, 'lib'))
         # there is a super weird bug, first import of pocketsphinx fails http://sourceforge.net/p/cmusphinx/bugs/284/
         write_file(os.path.join(robustus.env, 'lib/python2.7/site-packages/wrap_pocketsphinx.py'),
@@ -67,5 +58,8 @@ def install(robustus, requirement_specifier, rob_file, ignore_index):
                    + 'except:\n'
                    + '    pass\n'
                    + 'from pocketsphinx import *\n')
-    else:
-        raise RequirementException('can\'t find pocketsphinx-%s in robustus cache' % requirement_specifier.version)
+    except RequirementException:
+        safe_remove(build_dir)
+    finally:
+        safe_remove(archive)
+        os.chdir(cwd)
--- robustus/detail/install_sphinxbase.py
+++ robustus/detail/install_sphinxbase.py
@@ -12,10 +12,22 @@ from requirement import RequirementException
 def install(robustus, requirement_specifier, rob_file, ignore_index):
     build_dir = os.path.join(robustus.cache, 'sphinxbase-%s' % requirement_specifier.version)
 
-    def in_cache():
-        return os.path.isdir(build_dir)
+    # check if already installed
+    sphinxbase = os.path.join(robustus.env, 'lib/python2.7/site-packages/sphinxbase.so')
+    if os.path.isfile(sphinxbase):
+        return
 
-    def configure():
+    # unfortunately we can't cache sphinxbase, it has to be rebuild after reconfigure
+    cwd = os.getcwd()
+    try:
+        # build in cache
+        os.chdir(robustus.cache)
+        archive = robustus.download('sphinxbase', requirement_specifier.version)
+        unpack(archive)
+
+        logging.info('Building sphinxbase')
+        os.chdir(build_dir)
+
         retcode = run_shell('./configure'
                             + (' --prefix=%s' % robustus.env)
                             + (' --with-python=%s' % os.path.join(robustus.env, 'bin/python')),
@@ -24,43 +36,23 @@ def install(robustus, requirement_specifier, rob_file, ignore_index):
         if retcode != 0:
             raise RequirementException('sphinxbase configure failed')
 
-    cwd = os.getcwd()
-    if not in_cache() and not ignore_index:
-        try:
-            # build in cache
-            os.chdir(robustus.cache)
-            archive = robustus.download('sphinxbase', requirement_specifier.version)
-            unpack(archive)
+        # link python-config from system-wide installation, sphinxbase configure requires it
+        python_config = os.path.join(robustus.env, 'bin/python-config')
+        if not os.path.isfile(python_config):
+            ln('/usr/bin/python-config', python_config)
 
-            logging.info('Building sphinxbase')
-            os.chdir(build_dir)
+        retcode = run_shell(['make'], verbose=robustus.settings['verbosity'] >= 1)
+        if retcode != 0:
+            raise RequirementException('sphinxbase build failed')
 
-            configure()
-
-            # link python-config from system-wide installation, sphinxbase configure requires it
-            python_config = os.path.join(robustus.env, 'bin/python-config')
-            if not os.path.isfile(python_config):
-                ln('/usr/bin/python-config', python_config)
-
-            retcode = run_shell(['make'], verbose=robustus.settings['verbosity'] >= 1)
-            if retcode != 0:
-                raise RequirementException('sphinxbase build failed')
-        except RequirementException:
-            safe_remove(build_dir)
-        finally:
-            safe_remove(archive)
-            os.chdir(cwd)
-
-    if in_cache():
         logging.info('Installing sphinxbase into virtualenv')
-        os.chdir(build_dir)
-        configure() # need to configure, because install path may change
         retcode = run_shell('make install', shell=True, verbose=robustus.settings['verbosity'] >= 1)
-        os.chdir(cwd)
         if retcode != 0:
             raise RequirementException('sphinxbase install failed')
-        # fix rpath for sphinxbase
-        sphinxbase = os.path.join(robustus.env, 'lib/python2.7/site-packages/sphinxbase.so')
+
         fix_rpath(robustus.env, sphinxbase, os.path.join(robustus.env, 'lib'))
-    else:
-        raise RequirementException('can\'t find sphinxbase-%s in robustus cache' % requirement_specifier.version)
+    except RequirementException:
+        safe_remove(build_dir)
+    finally:
+        safe_remove(archive)
+        os.chdir(cwd)
