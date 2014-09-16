0add28ed211075e443abaa53a379a221e8462fa5
olegsinyavskiy <sinyavskiy@braincorporation.com>
2014-09-15 22:38:01 -0700

    comment

--- robustus/detail/install_protobuf.py
+++ robustus/detail/install_protobuf.py
@@ -16,6 +16,8 @@ def install(robustus, requirement_specifier, rob_file, ignore_index):
     os.chdir(robustus.cache)
 
     install_dir = os.path.join(robustus.cache, 'protobuf-%s' % requirement_specifier.version)
+    
+    # always recompile protobuf because it has been cached buggy
     # if not os.path.isdir(install_dir) and not ignore_index:
     if True:
         archive_name = 'protobuf-%s.tar.gz' % requirement_specifier.version
