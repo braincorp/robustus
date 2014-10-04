8b074d45450578f664e360a547a91ef5bca4040a
olegsinyavskiy <sinyavskiy@braincorporation.com>
2014-10-04 12:58:04 -0700

    do not chdir

--- robustus/detail/install_pyside.py
+++ robustus/detail/install_pyside.py
@@ -47,21 +47,21 @@ def install(robustus, requirement_specifier, rob_file, ignore_index):
         # Install through wheeling
         robustus.install_through_wheeling(requirement_specifier, rob_file, ignore_index)
     
-        # Need links to shared libraries
-        pyside_setup_dir = os.path.join(robustus.cache, 'pyside-setup-master')
-        if not os.path.isdir(pyside_setup_dir) and not ignore_index:
-            os.chdir(robustus.cache)
-            pyside_setup_archive = robustus.download('pyside-setup', 'master')
-            unpack(pyside_setup_archive)
-#     
-#         cwd = os.getcwd()
-#         try:
-#             # run postinstall
-#             if not os.path.isdir(pyside_setup_dir):
-#                 raise RequirementException('can\'t find pyside-%s in robustus cache' % requirement_specifier.version)
-#             os.chdir(pyside_setup_dir)
-#             retcode = subprocess.call([robustus.python_executable, 'pyside_postinstall.py', '-install'])
-#             if retcode != 0:
-#                 raise RequirementException('failed to execute pyside postinstall script')
-#         finally:
-#             os.chdir(cwd)
+        cwd = os.getcwd()
+        try:
+            # Need links to shared libraries
+            pyside_setup_dir = os.path.join(robustus.cache, 'pyside-setup-master')
+            if not os.path.isdir(pyside_setup_dir) and not ignore_index:
+                os.chdir(robustus.cache)
+                pyside_setup_archive = robustus.download('pyside-setup', 'master')
+                unpack(pyside_setup_archive)
+
+            # run postinstall
+            if not os.path.isdir(pyside_setup_dir):
+                raise RequirementException('can\'t find pyside-%s in robustus cache' % requirement_specifier.version)
+            os.chdir(pyside_setup_dir)
+            retcode = subprocess.call([robustus.python_executable, 'pyside_postinstall.py', '-install'])
+            if retcode != 0:
+                raise RequirementException('failed to execute pyside postinstall script')
+        finally:
+            os.chdir(cwd)
