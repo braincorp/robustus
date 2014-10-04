829a62057b75d810db51438d37c2381fb2a42df3
olegsinyavskiy <sinyavskiy@braincorporation.com>
2014-10-04 10:38:25 -0700

    no post install

--- robustus/detail/install_pyside.py
+++ robustus/detail/install_pyside.py
@@ -17,7 +17,6 @@ def install(robustus, requirement_specifier, rob_file, ignore_index):
     # If we are not on Travis, we only link to system-wide PySide (for now), and return
     # The reason we do this hack for now is to avoid installing qt sdk on bstem
     # So, on bstem and linux machines we will need to sudo apt-get install python-pyside
-    raise RequirementException('Sorrrrry!')
     if 'TRAVIS' not in os.environ: 
         # Create links to system-wide PySide
         if sys.platform.startswith('darwin'):
@@ -49,20 +48,20 @@ def install(robustus, requirement_specifier, rob_file, ignore_index):
         robustus.install_through_wheeling(requirement_specifier, rob_file, ignore_index)
     
         # Need links to shared libraries
-        pyside_setup_dir = os.path.join(robustus.cache, 'pyside-setup-master')
-        if not os.path.isdir(pyside_setup_dir) and not ignore_index:
-            os.chdir(robustus.cache)
-            pyside_setup_archive = robustus.download('pyside-setup', 'master')
-            unpack(pyside_setup_archive)
-    
-        cwd = os.getcwd()
-        try:
-            # run postinstall
-            if not os.path.isdir(pyside_setup_dir):
-                raise RequirementException('can\'t find pyside-%s in robustus cache' % requirement_specifier.version)
-            os.chdir(pyside_setup_dir)
-            retcode = subprocess.call([robustus.python_executable, 'pyside_postinstall.py', '-install'])
-            if retcode != 0:
-                raise RequirementException('failed to execute pyside postinstall script')
-        finally:
-            os.chdir(cwd)
+#         pyside_setup_dir = os.path.join(robustus.cache, 'pyside-setup-master')
+#         if not os.path.isdir(pyside_setup_dir) and not ignore_index:
+#             os.chdir(robustus.cache)
+#             pyside_setup_archive = robustus.download('pyside-setup', 'master')
+#             unpack(pyside_setup_archive)
+#     
+#         cwd = os.getcwd()
+#         try:
+#             # run postinstall
+#             if not os.path.isdir(pyside_setup_dir):
+#                 raise RequirementException('can\'t find pyside-%s in robustus cache' % requirement_specifier.version)
+#             os.chdir(pyside_setup_dir)
+#             retcode = subprocess.call([robustus.python_executable, 'pyside_postinstall.py', '-install'])
+#             if retcode != 0:
+#                 raise RequirementException('failed to execute pyside postinstall script')
+#         finally:
+#             os.chdir(cwd)
