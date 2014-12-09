926751f72175a7cea6b6b5d488bac0870ab7abed
Dmitry Trifonov <trifonov@braincorporation.com>
2014-12-09 09:34:45 -0800

    changed openni install script, allow '-' in requirement specifier

--- robustus/detail/install_openni.py
+++ robustus/detail/install_openni.py
@@ -6,45 +6,57 @@
 import logging
 import os
 import platform
-from utility import run_shell
+from utility import run_shell, cp
 from requirement import RequirementException
+import sys
 
 
 def install(robustus, requirement_specifier, rob_file, ignore_index):
     ni_install_dir = os.path.join(robustus.cache, 'OpenNI-%s' % requirement_specifier.version)
-    ni2so = os.path.join(ni_install_dir, 'lib/python2.7/site-packages/cv2.so')
+
     def in_cache():
-        return os.path.isfile(ni2so)
+        return os.path.isfile(os.path.join(ni_install_dir, 'README'))
 
     if not in_cache() and not ignore_index:
+        cwd = os.getcwd()
+        ni_clone_dir = os.path.join(cwd, 'OpenNI2')
+
         try:
-            logging.info('Cloning OpenNI')
-            retcode = run_shell(['git', 'clone', 'https://github.com/OpenNI/OpenNI2.git'])
-            if retcode != 0:
-                raise RequirementException('OpenNI clone failed')
-            ni_clone_dir = 'OpenNI2'
+            if os.path.isdir(ni_clone_dir):
+                logging.warn('Directory for cloning OpenNI found, cloning skipped')
+            else:
+                logging.info('Cloning OpenNI')
+                retcode = run_shell(['git', 'clone', 'https://github.com/occipital/OpenNI2.git'])
+                if retcode != 0:
+                    raise RequirementException('OpenNI clone failed')
             os.chdir(ni_clone_dir)
 
             # checkout requested version
-            retcode = run_shell(['git', 'checkout', 'tags/' + requirement_specifier.version])
-            if retcode != 0:
-                raise RequirementException('OpenNI checkout failed')
+            branch = requirement_specifier.version if requirement_specifier.version is not None else 'master'
+            if requirement_specifier.version is not None:
+                retcode = run_shell(['git', 'checkout', branch])
+                if retcode != 0:
+                    raise RequirementException('OpenNI checkout failed')
 
             logging.info('Building OpenNI')
-            if platform.machine().startswitch('arm'):
-                retcode = run_shell(['PLATFORM=Arm make'], verbose=robustus.settings['verbosity'] >= 1)
+            if platform.machine().startswith('arm'):
+                ver = 'arm'
+            elif platform.architecture()[0].startswith('64'):
+                ver = 'x64'
             else:
-                retcode = run_shell(['make', '-j4'], verbose=robustus.settings['verbosity'] >= 1)
-
+                ver = 'x86'
+            os.chdir('Packaging')
+            retcode = run_shell([sys.executable, 'ReleaseVersion.py', ver], verbose=robustus.settings['verbosity'] >= 1)
             if retcode != 0:
                 raise RequirementException('OpenNI build failed')
 
-                logging.info('Building OpenNI')
-                cv_build_dir = os.path.join(opencv_archive_name, 'build')
-                if not os.path.isdir(cv_build_dir):
-                    os.mkdir(cv_build_dir)
-                os.chdir(cv_build_dir)
+            # copy the whole dir to wheelhouse
+            if not os.path.isdir(ni_install_dir):
+                os.mkdir(ni_install_dir)
+            cp(ni_clone_dir + '/*', ni_install_dir)
         finally:
             #safe_remove(ni_clone_dir)
             os.chdir(cwd)
+
+    # run setup script from wheelhouse to env
 
