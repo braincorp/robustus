fc2a7ba4a6b62a50beff31549e7dbd2b8363b9c3
Dmitry Trifonov <trifonov@braincorporation.com>
2014-12-09 17:31:22 -0800

    check for libOpenNI2.so

--- robustus/tests/test_openni.py
+++ robustus/tests/test_openni.py
@@ -5,14 +5,19 @@
 
 import pytest
 from robustus.detail import perform_standard_test
+import sys
 
 
 def test_openni_installation(tmpdir):
     tmpdir.chdir()
     # ok if sensor is not connected, should fail if library not found
-    exprs = ['from primesense import openni2\nopenni2.initialize()']
+    exprs = []  #['from primesense import openni2\nopenni2.initialize()']
+    if sys.platform == "linux" or sys.platform == "linux2":
+        files = ['lib/libOpenNI2.so']
+    else:
+        files = []
     dependencies = ['OpenNI']
-    perform_standard_test('primesense==2.2.0.30-5', exprs, [], dependencies)
+    perform_standard_test('primesense==2.2.0.30-5', exprs, files, dependencies)
 
 
 if __name__ == '__main__':
