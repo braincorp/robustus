46ef743f24e59c5288e2484b8e4558178226126d
h366lj2 <c_jliu@braincorporation.com>
2018-02-22 18:06:51 -0800

    Added support for using "* master" as an indication of being the active branch.

--- robustus/robustus.py
+++ robustus/robustus.py
@@ -555,6 +555,7 @@ class Robustus(object):
                 os.chdir(directory)
                 msg =subprocess.check_output('git branch -rv --abbrev=40|grep $(git rev-parse HEAD)', shell=True)
                 active_branch = re.search('origin/\w*', msg).group(0)
+                active_branch |= re.search('^\\* master', msg).group(0)
                 # http://stackoverflow.com/questions/6657690/python-getoutput-equivalent-in-subprocess
             except Exception as err:
                 active_branch = ('<Could not find active branch - %s: %s>. Branch might be out-of-sync with origin. '
